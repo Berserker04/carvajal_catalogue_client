aea058e45476146f7d0897fc0732ef56107aa69a
Berserker04 <xcarlosdesarrolladorx@gmail.com>
2023-07-24 19:01:37 -0500

    reactive form for login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  form = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.minLength(6), Validators.required]],
+  });
 
+  loading: boolean = false;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  login(): void {
+    if (this.form.valid) {
+      this.loading = true;
+      const { email, password } = this.form.getRawValue();
+      console.log(email);
+      console.log(password);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
